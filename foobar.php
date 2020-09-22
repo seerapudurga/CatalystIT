f1ed9f24abbf85ffb31705cf028b727c2c17ad2c
seerapudurga <63042967+seerapudurga@users.noreply.github.com>
2020-09-22 22:25:34 +1000

    Final Commit
    Added comments and cleaned the code

--- foobar.php
+++ foobar.php
@@ -1,28 +1,27 @@
 <?php 
 
 $output = array();
-$y = 0;
-echo("\n");
+echo("\n"); 				
 for($i=1;$i<=100;$i++){
-	if($i%15 == 0){
-		//print_r("foobar");
+
+	if($i%15 == 0){        //Since both 3 and 5 are prime 3*5=15. So divisible by 3 and 5 are divisible by 15. 
 		$y = "foobar";
-	}else if($i%5 == 0){
-		//print_r("bar");
+	}
+	else if($i%5 == 0){   //checking if divisible by 3.
 		$y = "bar";
 	}
-	else if($i%3 == 0){
-		//print_r("foo");
+	else if($i%3 == 0){    //checking if divisible by 3.
 		$y = "foo";
-	}else{
-		//print_r($i);
-		$y = $i;
-	}	
-	echo(' ');
-	array_push($output, $y);
-	//$output[] = $i;
+	}else{					
+		$y = $i;		
+	}		
+	
+	array_push($output, $y); //Pushing the output into an array
+
 }
-print_r($output);
+
+	$comma_separated = implode(", ", $output);		//output as a string separated with comma 
+	print_r($comma_separated);						
 echo("\n");
 
 ?>

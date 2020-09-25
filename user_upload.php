fa0f37ccf076e40dea69397696fcbf492412cfda
seerapudurga <63042967+seerapudurga@users.noreply.github.com>
2020-09-25 11:07:17 +1000

    Error message check and code optimisation

--- user_upload.php
+++ user_upload.php
@@ -1,19 +1,94 @@
 <?php
 
+
+
+/****    	Database Functions 		  *****/
+
+//Database Connection .
+
+function OpenCon($db_username,$db_password,$db_host)
+ {
+	$mysqli = mysqli_init();  //initialising MySQLi
+	if (!$mysqli) {
+		die('mysqli_init failed');
+	}
+	if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+		die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+	}
+	if (!$mysqli->real_connect($db_host, $db_username, $db_password)) {  //if the connection fails returning error
+		die('Connect Error (' . mysqli_connect_errno() . ') '
+			. mysqli_connect_error());
+	}else
+		return $mysqli;
+ }
+ 
+ 
+ //Checking if Database Exists. If not Creating a new DB called Users .
+ function selectDB($conn){
+	$db_selected = $conn->select_db('catalyst_user');
+	if (!$db_selected) {
+	  $sql = 'CREATE DATABASE catalyst_user';
+	
+	  if ($conn->query($sql)) {
+		  echo "Database created successfully\n";
+	  } else {
+		  echo 'Error creating database: ' . $conn -> error . "\n";
+	  }
+	}
+	return $conn;
+}
+function create_table(){
+	echo "Database Username:";
+	$db_username = rtrim(fgets(STDIN));
+	echo "Database Password:";
+	$db_password = rtrim(fgets(STDIN));
+	echo "Database Hostname:";
+	$db_host = rtrim(fgets(STDIN));
+	$conn = OpenCon($db_username,$db_password,$db_host);
+	
+	
+	if(empty($conn->connect_error)){
+		$conn = selectDB($conn);
+		$sql = "CREATE TABLE IF NOT EXISTS `users` (
+			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			name VARCHAR(30) NOT NULL,
+			surname VARCHAR(30) NOT NULL,
+			email VARCHAR(50),
+			reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+		)";
+
+		if ($conn->query($sql)) {
+		  echo "Table created successfully\n";
+	  } else if ($conn->error) {
+		  echo "Error creating table: " . $conn->error;
+		}
+		return $conn;
+	}else {
+		  echo 'Error Connecting to database: ' . $conn->connect_error . "\n";
+	  }
+
+}
+
+
 function insertData($conn, $records){
 
+	$dbrecord = [];
+	$i=0;
 	foreach($records as $record){
 		$name = $conn->real_escape_string($record['name']);
 		$surname = $conn->real_escape_string($record['surname']);
-		$sql = "INSERT INTO guess (name, surname, email)
+		$sql = "INSERT INTO users (name, surname, email)
 		VALUES ('$name', '$surname', '$record[email]')";
 
 		if ($conn->query($sql) === TRUE) {
+			$dbrecord[$i++] = $record['name'].', '.$record['surname'].','.$record['email'];
 		  continue;
 		} else {
 		  echo "Error: " . $sql . "<br>" . $conn->error;
 		}
+		
 	}
+	return $dbrecord;
 }
 
 function dataChecking($data){
@@ -25,16 +100,14 @@ function dataChecking($data){
 		$record['name'] = ucfirst(strtolower(trim($record['name'])));
 		$record['surname'] = ucfirst(strtolower(trim($record['surname'])));
 		$record['email'] = strtolower(trim($record['email']));
-		$x = $record['email'];
-		if ((!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $x))) {
+		if ((!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $record['email']))) {
 			$emailErr = "Invalid email format:  ".$record['email'];
 			break;
 		}else{
 			$valid_records[] = $record;
 		}
 	}
-	return array('valid_records'=>$valid_records,'emailErr'=>$emailErr);
-	print_r($valid_records);
+	return array('valid_records'=>$valid_records,'error'=>$emailErr);
 }
 
 function csvToArray($filename){
@@ -76,78 +149,10 @@ function csvToArray($filename){
 
 }
 
-function OpenCon($db_username,$db_password,$db_host)
- {
-	$mysqli = mysqli_init();
-	if (!$mysqli) {
-		die('mysqli_init failed');
-	}
-	if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
-		die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
-	}
-	if (!$mysqli->real_connect($db_host, $db_username, $db_password)) {
-		die('Connect Error (' . mysqli_connect_errno() . ') '
-			. mysqli_connect_error());
-	}else
-		return $mysqli;
- }
- 
- function selectDB($conn){
-	$db_selected = $conn->select_db('my_db');
-	if (!$db_selected) {
-	  $sql = 'CREATE DATABASE my_db';
-	
-	  if ($conn->query($sql)) {
-		  echo "Database my_db created successfully\n";
-	  } else {
-		  echo 'Error creating database: ' . $conn -> error . "\n";
-	  }
-	}
-	return $conn;
-}
-function create_table(){
-	echo "Database Username:";
-	$db_username = rtrim(fgets(STDIN));
-	echo "Database Password:";
-	$db_password = rtrim(fgets(STDIN));
-	echo "Database Hostname:";
-	$db_host = rtrim(fgets(STDIN));
-	$conn = OpenCon($db_username,$db_password,$db_host);
-	
-	
-	if(empty($conn->connect_error)){
-		$conn = selectDB($conn);
-		$sql = "CREATE TABLE IF NOT EXISTS `guess` (
-			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-			name VARCHAR(30) NOT NULL,
-			surname VARCHAR(30) NOT NULL,
-			email VARCHAR(50),
-			reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-		)";
-
-		if ($conn->error) {
-		  echo "Error creating table: " . $conn->error;
-		}
-		return $conn;
-	}else {
-		  echo 'Error Connecting to database: ' . $conn->connect_error . "\n";
-	  }
-
-}
 
 function helpPrint()
 {
-	/*$help_data = Array(
-		'--file' = > 'Name of the CSV to be parsed',
-		'--create_table' = > 'Creates MySql table',
-		'--dry_run' = > 'Run with --file directive to run the script but not insert into the DB',
-		'--help' = > 'Help',
-		'-u' = > 'MySql username',
-		'-p' = > 'MySql password',
-		'-h' = > 'MySql host'
-		);
-*/
-		
+
 	echo "\n"."--file [csv filename] \t Name of the CSV to be parsed";
 	echo "\n"."--create_table \t\t Creates MySql table";
 	echo "\n"."--dry_run \t\t Run with --file directive (--dry_run --file [csv filename])";
@@ -157,44 +162,65 @@ function helpPrint()
 	echo "\n"."--help \t\t\t help";
 
 }
-
-
-
-unset($argv[0]);
-
-$connection = [];
-	switch ($argv[1]) {
-	  case "--file":
-		$result_arr = csvToArray($argv[2]);
-		if($result_arr){
+function filedirective($filename){
+	$result_arr = csvToArray($filename);
+	
+		
+		if(count($result_arr['valid_records'])>0){
 			$conn = create_table();
 			if($conn->error == ""){
 				$data = insertData($conn, $result_arr['valid_records']);
+				if(!empty($data)){
+					echo '--------------------------------';
+					echo "\nRecords Inserted in Database:";
+					echo "\n";
+					print_r(implode("\n",$data));
+				}
+				
 			}else{
 				echo "Error: ".$conn->error;
 			}
-		}
-		break;
-	  case "--create_table":
-		$conn = create_table();
-		break;
-	  case "--dry_run":
-		$result_arr = csvToArray($argv[3]);
+		} 
+
 		if(array_key_exists('error', $result_arr)){
-			echo "\nError While processing file: ".$result_arr['error'];
+			echo "\n\nError Occurred: ".$result_arr['error'];
 		}
-		else if($result_arr['emailErr']){
-			echo $result_arr['emailErr'];
-		}else{
+		
+}
+
+function dryRun($filename){
+	$result_arr = csvToArray($filename);
+		if(array_key_exists('error', $result_arr)){
+			echo "\nError Occurred: ".$result_arr['error'];
+		} else{
 			echo "Records are validated. No errors found";
 		}
+	
+}
+
+/**********		Execution Starts from here		***********/
+
+	switch ($argv[1]) {
+	
+	  case "--file":
+		filedirective($argv[2]);
 		break;
-	  case "-u":
-		echo "--u";
+		
+	  case "--create_table":
+		$conn = create_table();
+		if($conn->error == ""){
+			echo 'Command Executed Successfully';
+		}
 		break;
+		
+	  case "--dry_run":
+		dryRun($argv[3]);
+		break;
+		
 	  case "--help":
 		helpPrint();
 		break;
+		
 	  default:
 		helpPrint();
 	}
